0ef7f5b43df1c273d644ca31c602e393c66a0411
Leonid <leonidg34@gmail.com>
2025-01-17 18:15:25 +0100

    implementation of gestures

--- src/hand_tracking/tracker.py
+++ src/hand_tracking/tracker.py
@@ -22,7 +22,70 @@ class HandTracker:
           frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
         )
 
+        self.recognize_gestures(hand_landmarks)
+
     return frame, results
   
+  def recognize_gestures(self, hand_landmarks):
+    if self.is_open_hand(hand_landmarks.landmark):
+      print("Open hand")
+
+    if self.is_fist(hand_landmarks.landmark):
+      print("Closed hand")
+
+    if self.is_custom_gesture(hand_landmarks.landmark):
+      print("Test")
+
+  def is_open_hand(self, landmarks):
+    tips = [4, 8, 12, 16, 20]
+    open_fingers = 0
+
+    for tip in tips:
+      if landmarks[tip].y < landmarks[tip - 2].y:   # Fingertip above the joint
+        open_fingers += 1
+
+    return open_fingers == 5
+  
+  def is_fist(self, landmarks):
+    tips = [4, 8, 12, 16, 20]
+    base_joints = [1, 5, 9, 13, 17]
+    closed_fingers = 0
+    for tip, base_joint in zip(tips, base_joints):
+      if landmarks[tip].y > landmarks[base_joint].y:   # Fingertip below the joint
+        closed_fingers += 1
+
+    return closed_fingers == 5
+  
+  def is_custom_gesture(self, landmarks):
+    open_fingers = 0
+    closed_fingers = 0
+
+    if landmarks[4].y < landmarks[1].y:
+      open_fingers += 1
+    else:
+      closed_fingers += 1
+
+    if landmarks[8].y > landmarks[5].y:
+      open_fingers += 1
+    else:
+      closed_fingers += 1
+
+    if landmarks[12].y > landmarks[9].y:
+      open_fingers += 1
+    else:
+      closed_fingers += 1
+
+    if landmarks[16].y > landmarks[13].y:
+      open_fingers += 1
+    else:
+      closed_fingers += 1
+
+    if landmarks[20].y > landmarks[17].y:
+      open_fingers += 1
+    else:
+      closed_fingers += 1
+
+    return open_fingers == 2 and closed_fingers == 3
+
   def release(self):
     self.hands.close()
